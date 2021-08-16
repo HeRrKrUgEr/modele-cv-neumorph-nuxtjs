73af10b54225d8a891548df81ca62020ef83852c
HeRrKrUgEr <vchanliau@gmail.com>
2021-08-16 02:02:27 +0200

    misc changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,6 +70,7 @@ export default {
               'faCog',
               'faFilePdf',
               'faInfo',
+              'faCloudDownload',
             ],
           },
           {
--- pages/index.vue
+++ pages/index.vue
@@ -30,42 +30,68 @@
                 ><span class="text-lg">Informations</span>
               </div>
 
-              <div class="flex flex-wrap mt-4 px-2 md:px-4">
-                <div class="mb-2 text-xl w-1/2">
+              <div class="flex flex-col mt-4 px-2 md:px-8">
+                <div
+                  class="mb-4 text-2xl inline-flex justify-start align-middle"
+                >
                   <fa
                     :icon="['fal', 'birthday-cake']"
                     class="fa-fw mr-3 text-cv-accent"
-                  ></fa
-                  ><span class="text-sm">14 août 1980 - {{ age }} ans</span>
+                  ></fa>
+
+                  <span class="text-lg font-light"
+                    >14 août 1980 - {{ age }} ans</span
+                  >
                 </div>
-                <div class="mb-3 text-xl w-1/2">
+                <div
+                  class="mb-4 text-2xl inline-flex justify-start align-middle"
+                >
                   <fa
                     :icon="['fal', 'home']"
                     class="fa-fw mr-3 text-cv-accent"
-                  ></fa
-                  ><span class="text-sm">Nancy / Strasbourg</span>
+                  ></fa>
+
+                  <span class="text-lg font-light">Alsace - Lorraine</span>
                 </div>
-                <div class="w-1/2">
-                  <a href="mailto:vchanliau@gmail.com" class="fa-link text-sm"
-                    ><fa
+                <div class="mb-4">
+                  <a
+                    href="mailto:vchanliau@gmail.com"
+                    class="fa-link text-lg inline-flex justify-start align-middle"
+                  >
+                    <fa
                       :icon="['fal', 'envelope']"
-                      class="fa-fw mr-3 text-cv-accent text-xl"
-                    ></fa
-                    >vchanliau@gmail.com</a
+                      class="fa-fw mr-3 text-cv-accent text-2xl"
+                    ></fa>
+
+                    <span class="text-lg">vchanliau@gmail.com</span></a
                   >
                 </div>
-                <div class="w-1/2">
-                  <a href="tel:+33611826310" class="fa-link text-sm">
+                <div class="mb-4">
+                  <a href="tel:+33611826310" class="fa-link text-lg">
                     <fa
                       :icon="['fal', 'phone-rotary']"
-                      class="fa-fw mr-3 text-cv-accent text-xl"
-                    ></fa
-                    >06.11.82.63.10</a
+                      class="fa-fw mr-3 text-cv-accent text-2xl"
+                    ></fa>
+
+                    <span class="text-lg">06.11.82.63.10</span></a
+                  >
+                </div>
+                <div class="mb-4">
+                  <a
+                    href="cv-vincentchanliau-2021.pdf"
+                    class="fa-link text-lg inline-flex justify-start align-middle"
+                  >
+                    <fa
+                      :icon="['fal', 'cloud-download']"
+                      class="fa-fw mr-3 text-cv-accent text-2xl"
+                    ></fa>
+
+                    <span class="text-lg">Télécharger mon CV</span></a
                   >
                 </div>
               </div>
 
-              <div class="flex justify-center my-4">
+              <div class="flex justify-center mt-2 px-2 md:px-8">
                 <a
                   href="https://www.facebook.com/vincent.chanliau/"
                   target="_blank"
@@ -108,16 +134,6 @@
                   </svg>
                 </a>
               </div>
-              <div class="flex justify-center">
-                <a
-                  href="cv-vincentchanliau-2021.pdf"
-                  target="_blank"
-                  role="button"
-                  class="download-btn"
-                >
-                  Télécharger mon CV
-                </a>
-              </div>
             </div>
             <div>
               <div
@@ -148,7 +164,7 @@
                   ></fa
                   >Développement d'applications et sites Web
                 </p>
-                <p class="text-base mb-2 font-light">
+                <p class="text-base font-light">
                   <fa
                     :icon="['fal', 'asterisk']"
                     class="mr-2 text-cv-accent text-xs"
@@ -200,16 +216,16 @@
                 <fa :icon="['fal', 'globe']" class="fa-fw mr-3"></fa
                 ><span class="text-lg">Langues étrangères</span>
               </div>
-              <div class="px-2 md:px-4">
-                <div class="flex flex-row items-center justify-start ml-4 mt-4">
+              <div class="px-2 md:px-8">
+                <div class="flex flex-row items-center justify-start mt-4">
                   <img src="img/uk.png" class="mr-3 shadow-md" />
                   <p class="text-base font-light">Anglais (bilingue)</p>
                 </div>
-                <div class="flex flex-row items-center justify-start ml-4 mt-4">
+                <div class="flex flex-row items-center justify-start mt-4">
                   <img src="img/indonesia.png" class="mr-3 shadow-md" />
                   <p class="text-base font-light">Indonésien (intermédiaire)</p>
                 </div>
-                <div class="flex flex-row items-center justify-start ml-4 mt-4">
+                <div class="flex flex-row items-center justify-start mt-4">
                   <img src="img/spain.png" class="mr-3 shadow-md" />
                   <p class="text-base font-light">Espagnol (basique)</p>
                 </div>
@@ -222,7 +238,7 @@
                 <fa :icon="['fal', 'golfball']" class="fa-fw mr-3"></fa
                 ><span class="text-base">Hobbies</span>
               </div>
-              <div class="px-2 md:px-4 pb-4">
+              <div class="px-2 md:px-8 pb-8">
                 <p
                   class="inline-block mx-3 pl-3 pr-4 py-1 bg-cv-accent text-white rounded-xl text-center"
                 >
@@ -230,7 +246,7 @@
                     :icon="['fal', 'golfclub']"
                     class="fa-fw mr-2 text-white"
                   ></fa
-                  ><span class="text-sm font-medium">Golf</span>
+                  ><span class="text-base font-medium">Golf</span>
                 </p>
                 <p
                   class="inline-block mx-3 pl-3 pr-4 py-1 bg-cv-accent text-white rounded-xl text-center"
@@ -239,7 +255,7 @@
                     :icon="['fal', 'piano']"
                     class="fa-fw mr-2 text-white"
                   ></fa
-                  ><span class="text-sm font-medium">Piano</span>
+                  ><span class="text-base font-medium">Piano</span>
                 </p>
               </div>
             </div>
@@ -270,11 +286,11 @@
                 >
                 / Bali, Indonésie
               </h5>
-              <h6 class="text-gray-500 font-light mb-2 ml-4">
+              <h6 class="text-gray-600 font-light mb-2 ml-4">
                 <fa :icon="['fal', 'calendar']" class="fa-fw mr-2"></fa
                 ><span>Depuis avril 2009</span>
               </h6>
-              <p class="font-light text-justify text-sm mx-2">
+              <p class="font-light text-justify text-base mx-2">
                 Responsable du développement informatique des agences de
                 tourisme
                 <a target="_blank" href="https://www.unmondeautrement.com"
@@ -301,11 +317,11 @@
                 >
                 / Société Big Foot / Bali Indonésie
               </h5>
-              <h6 class="text-gray-500 font-light mb-2 ml-4">
+              <h6 class="text-gray-600 font-light mb-2 ml-4">
                 <fa :icon="['fal', 'calendar']" class="fa-fw mr-2"></fa
                 ><span>Décembre 2008 - Mars 2009</span>
               </h6>
-              <p class="font-light text-justify text-sm mx-2">
+              <p class="font-light text-justify text-base mx-2">
                 Chef de projet, responsable d'une équipe de 4 programmeurs dans
                 le cadre du développement d'un projet de portail B2B pour les
                 hôtels et resorts d'Asie. Analyse et mise en place des
@@ -323,11 +339,11 @@
                 <a href="https://www.kitview.com" target="_blank">KitView</a> /
                 Strasbourg, France
               </h5>
-              <h6 class="text-gray-500 font-light mb-2 ml-4">
+              <h6 class="text-gray-600 font-light mb-2 ml-4">
                 <fa :icon="['fal', 'calendar']" class="fa-fw mr-2"></fa
                 ><span>Septembre 2007 - novembre 2008</span>
               </h6>
-              <p class="font-light text-justify text-sm mx-2">
+              <p class="font-light text-justify text-base mx-2">
                 Responsable technique et commercial de la région Grand-Est pour
                 la société
                 <a href="https://www.kitview.com" target="_blank"
@@ -349,11 +365,11 @@
                 >
                 / Freelance / Montréal, Canada
               </h5>
-              <h6 class="text-gray-500 font-light mb-2 ml-4">
+              <h6 class="text-gray-600 font-light mb-2 ml-4">
                 <fa :icon="['fal', 'calendar']" class="fa-fw mr-2"></fa
                 ><span>Novembre 2004 - Juin 2007</span>
               </h6>
-              <p class="font-light text-justify text-sm">
+              <p class="font-light text-justify text-base">
                 Développeur de logiciels et de sites web en freelance.
               </p>
               <hr class="mt-4 mb-6" />
@@ -365,11 +381,11 @@
                 >
                 / Diatélic / Nancy, France
               </h5>
-              <h6 class="text-gray-500 font-light mb-2 ml-4">
+              <h6 class="text-gray-600 font-light mb-2 ml-4">
                 <fa :icon="['fal', 'calendar']" class="fa-fw mr-2"></fa
                 ><span>Septembre 2003 - Octobre 2004</span>
               </h6>
-              <p class="font-light text-justify text-sm mx-2">
+              <p class="font-light text-justify text-base mx-2">
                 Chargé du développement d'un logiciel de gestion de planning
                 pour le service de néphrologie du CHU de Brabois. Développement
                 du site web de la société Diatélic, précurseuse dans la
@@ -385,11 +401,11 @@
                 >
                 / Qualipse / Strasbourg, France
               </h5>
-              <h6 class="text-gray-500 font-light mb-2 ml-4">
+              <h6 class="text-gray-600 font-light mb-2 ml-4">
                 <fa :icon="['fal', 'calendar']" class="fa-fw mr-2"></fa
                 ><span>Septembre 2001 - Juillet 2003</span>
               </h6>
-              <p class="font-light text-justify text-sm mx-2">
+              <p class="font-light text-justify text-base mx-2">
                 Responsable réseau dans le cadre d'études en alternance en BTS
                 informatique de gestion.
               </p>
@@ -410,7 +426,7 @@
                 >
                 / IESA / Strasbourg, France
               </h5>
-              <h6 class="text-gray-500 font-light mb-2 ml-4">
+              <h6 class="text-gray-600 font-light mb-2 ml-4">
                 <fa :icon="['fal', 'calendar']" class="fa-fw mr-2"></fa
                 ><span>Septembre 2001 à Juillet 2003</span>
               </h6>
@@ -423,7 +439,7 @@
                 >
                 / Institut Horizon / Strasbourg, France
               </h5>
-              <h6 class="text-gray-500 font-light mb-2 ml-4">
+              <h6 class="text-gray-600 font-light mb-2 ml-4">
                 <fa :icon="['fal', 'calendar']" class="fa-fw mr-2"></fa
                 ><span>Année 2000</span>
               </h6>
@@ -508,11 +524,11 @@ body {
 }
 
 .download-btn {
-  padding: 10px 15px;
+  padding: 0.5rem 1rem;
   background-color: #e2e8f0;
   box-shadow: -5px -5px 10px rgba(255, 255, 255, 0.7),
     5px 5px 10px rgba(0, 0, 0, 0.2);
-  text-transform: uppercase;
+  //text-transform: uppercase;
   letter-spacing: 0.5px;
   font-size: 1rem;
   font-weight: bold;
@@ -523,6 +539,9 @@ body {
   line-height: 1;
   border-radius: 15px;
   transition: all 0.2s ease-in-out;
+  svg path {
+    fill: #f40f02;
+  }
   &:hover {
     color: rgba(0, 127, 171, 0.3);
     box-shadow: -1px -1px 2px rgba(255, 255, 255, 0.7),
@@ -576,28 +595,40 @@ body {
 }
 
 .fa-link {
-  width: 100%;
-  transition: all 0.25s linear;
+  transition: all 0.25s ease-in-out;
   border-radius: 5px;
+  font-weight: 300;
+  text-shadow: -1px 1px 1px rgba(0, 0, 0, 0.1),
+    1px -1px 1px rgba(255, 255, 255, 0.1);
   svg {
-    transition: all 0.25s linear;
+    transition: all 0.25s ease-in-out;
+    filter: drop-shadow(-1px 1px 1px rgba(0, 0, 0, 0.1))
+      drop-shadow(1px -1px 1px rgba(255, 255, 255, 0.1));
+    path {
+      transition: all 0.25s ease-in-out;
+    }
   }
   &:hover {
-    color: #007fab;
+    text-shadow: -2px 2px 2px rgba(0, 0, 0, 0.5),
+      2px -2px 2px rgba(255, 255, 255, 1);
     svg {
-      transform: scale(1.1) translatex(5px);
+      filter: drop-shadow(-1px 1px 1px rgba(0, 0, 0, 0.5))
+        drop-shadow(1px -1px 1px rgba(255, 255, 255, 0.5));
+      path {
+        fill: #e2e8f0;
+      }
     }
   }
 }
 
 .neu-left {
   box-shadow: 8px 8px 12px 0 rgba(0, 0, 0, 0.2),
-    -12px -12px 16px 0 rgba(255, 255, 255, 0.6);
+    -12px -12px 20px 0 rgba(255, 255, 255, 0.4);
 }
 
 .neu-right {
   box-shadow: -8px 8px 12px 0 rgba(0, 0, 0, 0.2),
-    12px -12px 16px 0 rgba(255, 255, 255, 0.6);
+    12px -12px 16px 0 rgba(255, 255, 255, 0.4);
 }
 
 .social-link {
